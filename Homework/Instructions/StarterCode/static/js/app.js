526a637990f9fac8570d4c6031cc1c81c0c16e9f
Nithin Sunil <nithinsunil@Nithins-MBP.attlocal.net>
2020-02-08 21:25:52 -0500

    final

--- Homework/Instructions/StarterCode/static/js/app.js
+++ Homework/Instructions/StarterCode/static/js/app.js
@@ -4,38 +4,56 @@ var tableData = data;
 // YOUR CODE HERE!
 var tbody = d3.select("tbody");
 console.log(data);
+popTable(tableData);
 
-data.forEach(function(weatherReport) {
-    console.log(weatherReport);
+
+function popTable(stuff) {
+  tbody.html("");
+  console.log("made it");
+  //d3.select("tbody").remove();
+
+stuff.forEach(function(weatherReport) {
+    //console.log(weatherReport);
     
     var row = tbody.append("tr");
+  
     Object.entries(weatherReport).forEach(function([key, value]) {
-      console.log(key, value);
+      //console.log(key, value);
       // Append a cell to the row for each value
       // in the weather report object
       var cell = row.append("td");
        cell.text(value);
      });
   });
+}
 
   
-  var button= d3.select('');
 
  // d3.selectAll("#filter-btn").on("click", button_clicked);
   d3.selectAll("#filter-btn").on("click", handleClick);
 
   function handleClick() {
-    // Grab the datetime value from the filter
-    var date = d3.select("#datetime").property("value");
+    // Grab the datetimeState value from the filter
+    var dateState = d3.select("#datetimeState").property("value");
     let filteredData = tableData;
+    console.log(dateState);
+    if (dateState.length===2 ) {
+      filteredData = filteredData.filter(row => row.state=== dateState);
+      console.log("Made it inside filter");
+    }
     // Check to see if a date was entered and filter the
     // data using that date.
-    if (date) {
+    else if (dateState.length>2) {
       // Apply `filter` to the table data to only keep the
-      // rows where the `datetime` value matches the filter value
-      filteredData = filteredData.filter(row => row.datetime === date);
-      console.log(filteredData);
+      // rows where the `datetimeState` value matches the filter value
+      filteredData = filteredData.filter(row => row.datetime === dateState);
+
     }
+    else {
+      return;
+    }
+    console.log(filteredData);
+    popTable(filteredData);
   }
 
 
@@ -43,9 +61,11 @@ data.forEach(function(weatherReport) {
 
   // function button_clicked() {
   //   console.log('here!')
-  //   var date= d3.select('#datetime');
+  //   var date= d3.select('#datetimeState');
   //   let filtered_data= tableData;
-  //   filtered_data=filtered_data.filter(row => row.datetime === date);
+  //   filtered_data=filtered_data.filter(row => row.datetimeState === date);
   //   console.log(filtered_data);
 
   // }
+
+  
